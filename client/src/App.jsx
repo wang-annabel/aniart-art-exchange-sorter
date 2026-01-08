d5c02c956f46ca283e40f96afbd672f440227f1b
Annabel Wang <annabew3@uci.edu>
2026-01-08 15:30:25 -0800

    basic ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import Graph from "./Graph";
+import Cards from "./Cards";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  // eventually set this to retrieve matchings from local storage
+  const [matchings, setMatchings] = useState([]);
 
   return (
     <>
-      <Graph width={400} height={400} />
+      <div className="content">
+        <h1>Art Exchange Sorter</h1>
+        <button className="upload-btn">
+          <span>
+            <img id="upload-btn-icon" src="\upload.png" />
+          </span>
+          <span>Upload CSV</span>
+        </button>
+      </div>
+      <div id="content">
+        {matchings.length == 0 && <Cards />}
+        {matchings.length > 0 && <Graph width={400} height={400} />}
+      </div>
     </>
   );
 }
---
+++ client/src/Cards.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function Cards() {
+  return (
+    <>
+      <div className="cards">
+        <div className="card">
+          <h2>1</h2>
+          <h3>Upload CSV</h3>
+          <p>From this form template, convert form responses to csv.</p>
+        </div>
+        <div className="card">
+          <h2>2</h2>
+          <h3>Review Matching</h3>
+          <p>From this form template, convert form responses to csv.</p>
+        </div>
+        <div className="card">
+          <h2>3</h2>
+          <h3>Confirm Matching</h3>
+          <p>From this form template, convert form responses to csv.</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
